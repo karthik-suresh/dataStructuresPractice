c9fb299297140186809e3fa57ae3b522dc8a2ccc
Karthik Hanasoge Suresh <kali.karthik@gmail.com>
2019-04-06 16:22:29 +0200

    Collections
    Q demo

---
+++ qQuiz.py
@@ -0,0 +1,12 @@
+class Queue(object):
+    def __init__(self, head=None):
+        self.storage = [head]
+
+    def enqueue(self, new_element):
+        self.storage.append(new_element)
+
+    def peek(self):
+        return self.storage[0]
+
+    def dequeue(self):
+        return self.storage.pop(0)
